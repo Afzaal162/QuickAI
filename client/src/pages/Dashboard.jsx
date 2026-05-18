557ee05a9bbb2030aa37391cd8fa4e8e7503a33f
Afzaal Hassan <afzaalhassan162@gmail.com>
2026-05-18 13:57:59 +0500

    fixes errors

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -5,8 +5,11 @@ import CreationItem from '../components/CreationItem'
 import axios from 'axios'
 import toast from 'react-hot-toast'
 
-axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
+axios.defaults.baseURL = import.meta.env.DEV 
+  ? 'http://localhost:3000' 
+  : 'https://quick-ai-server-7fzzdc43e-afzaal-hassans-projects.vercel.app';
 
+axios.defaults.withCredentials = true;
 const Dashboard = () => {
   const [creations, setCreations] = useState([]);
   const [loading, setLoading] = useState(true);
